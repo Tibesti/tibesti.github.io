6adb09e864a8b8856fbbc560b8528698c78b716e
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-05-05 03:40:49 -0600

    Re added pages
    I deleted this whole repo and created a new

---
+++ tomspurse/assets/js/main.js
@@ -0,0 +1,19 @@
+<<<<<<< Updated upstream:Tompurse/assets/js/main.js
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '80%';
+    document.getElementById('side-menu').style.height = '400px';
+    document.getElementById('side-menu').style.margin = '0 20px 0';
+}
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+}
+=======
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '320px';
+    document.getElementById('side-menu').style.height = '280px';
+    document.getElementById('side-menu').style.margin = '0 20px 0';
+}
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+}
+>>>>>>> Stashed changes:tomspurse/assets/js/main.js
