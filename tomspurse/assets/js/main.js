5ae2e717c78fd490b110f18af530eb2746e880d9
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-05-06 13:01:41 +0100

    Update main.js

--- tomspurse/assets/js/main.js
+++ tomspurse/assets/js/main.js
@@ -1,7 +1,7 @@
 
 function openSlideMenu() {
     document.getElementById('side-menu').style.width = '320px';
-    document.getElementById('side-menu').style.height = '280px';
+    document.getElementById('side-menu').style.height = '330px';
     document.getElementById('side-menu').style.margin = '0 20px 0';
 }
 function closeSlideMenu() {
