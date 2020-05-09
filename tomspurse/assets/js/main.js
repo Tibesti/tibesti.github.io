6a89da601745aca8f3b1cbdc8fbb5b7ddf3c09b8
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-05-08 22:39:01 -0600

    new update

--- tomspurse/assets/js/main.js
+++ tomspurse/assets/js/main.js
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream:Tompurse/assets/js/main.js
-function openSlideMenu() {
-    document.getElementById('side-menu').style.width = '80%';
-    document.getElementById('side-menu').style.height = '400px';
-    document.getElementById('side-menu').style.margin = '0 20px 0';
-}
-function closeSlideMenu() {
-    document.getElementById('side-menu').style.width = '0';
-}
-=======
 function openSlideMenu() {
     document.getElementById('side-menu').style.width = '320px';
     document.getElementById('side-menu').style.height = '280px';
@@ -16,4 +6,3 @@ function openSlideMenu() {
 function closeSlideMenu() {
     document.getElementById('side-menu').style.width = '0';
 }
->>>>>>> Stashed changes:tomspurse/assets/js/main.js
