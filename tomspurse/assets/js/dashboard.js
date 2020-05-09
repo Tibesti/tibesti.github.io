4d46c6a9942c37428034f3be1ad00dd0386af6c6
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-05-09 11:22:06 -0600

    dashboard menu update
    finished dashboard menu and added more specificity to hovered cards in the dashboard

--- tomspurse/assets/js/dashboard.js
+++ tomspurse/assets/js/dashboard.js
@@ -5,13 +5,45 @@ $(document).ready(function(){
             $(this).fadeOut(200, function(){
                 $(this).css({"background-color": "#022B69", "color": "white"}).fadeIn(200);
             });
+            $(this).css({"margin-top": "0px", "margin-left": "0px", "margin-right": "0px"});
         },
         function(){
             $(this).fadeOut(200, function(){
                 $(this).css({"background-color": "white", "color": "#022B69"}).fadeIn(200);
             });
+            $(this).css({"margin-top": "5px", "margin-left": "5px", "margin-right": "5px"});
         }
     );
+    $('#mobile-drpdown').click(function(){
+        $('#content-drop').toggle()
+    });
+    
+    $(".bg103").hover(
+        function(){
+            $(".bg103").css({"width": "100%"});
+            $(".bg104").css({"width": "100%"});
+            $('.side-menu-text').show();
+        },
+        function(){
+            $(".bg103").css({"width": "80px"});
+            $(".bg104").css({"width": "80px"});
+            $('.side-menu-text').hide();
+        }
+    );
+    
+    $('.bg104').hover(
+        function(){
+            $(".bg103").css({"width": "100%"});
+            $(".bg104").css({"width": "100%"});
+            $('.side-menu-text').show();
+        },
+        function(){
+            $(".bg103").css({"width": "80px"});
+            $(".bg104").css({"width": "80px"});
+            $('.side-menu-text').hide();
+        }
+    );
+
     $('#mobile-drpdown').click(function(){
         $('#content-drop').toggle()
     });
